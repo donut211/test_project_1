84d4884d752f8c12947ecb1e662aaa53eea03e3a
donut211 <andrewlasren10@gmail.com>
2019-10-25 14:23:22 -0500

    Commit for printfPractice.java

---
+++ test_project_1/src/printfPractice.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+
+public class printfPractice {
+  public static void main(String[] args) {
+	out.printf("%s%d%s", ">>",7,"<<\n");
+	out.printf("%s%6d%s", ">>",7,"<<\n");
+	out.printf("%s%d%6s", ">>",7, "<<\n");
+	out.printf("%s%d%s", ">>", 7,"<<\n");
+	
+	
+	
+
+			
+	
+}
+
+}
